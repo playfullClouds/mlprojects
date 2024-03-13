2c0ac4456a2e98308242311e12c6aa0cc50076db
playfullClouds <onyedikarije@gmail.com>
2024-03-13 03:28:59 -0400

    HyperParameter

--- BlackFriday/src/components/model_trainer.py
+++ BlackFriday/src/components/model_trainer.py
@@ -26,20 +26,77 @@ class ModelTrainerConfig:
     trained_model_file_path = os.path.join("artifacts", "model.pkl")
     
    
+# class ModelTrainer:
+#     def __init__(self):
+#         self.model_trainer_config = ModelTrainerConfig()
+        
+#     def get_best_model_from_report(self, model_report):
+#         # Extract only validation scores and model names
+#         validation_scores = {key.replace('_validation_score', ''): score for key, score in model_report.items() if '_validation_score' in key}
+       
+#         # Find the model with the highest validation score
+#         best_model_name = max(validation_scores, key=validation_scores.get)
+#         best_model_score = validation_scores[best_model_name]
+        
+#         return best_model_name, best_model_score
+    
+#     def initiate_model_trainer(self, train_array, validate_array):
+        
+#         try:
+#             logging.info("Split training, validation, and test input data")
+            
+#             X_train, y_train = train_array[:, :-1], train_array[:, -1]
+#             X_validate, y_validate = validate_array[:, :-1], validate_array[:, -1]
+
+#             models = {
+#                 "Random Forest": RandomForestRegressor(),
+#                 "Gradient Boosting": GradientBoostingRegressor(),
+#                 "XGBRegressor": XGBRegressor(),
+#                 "CatBoosting Regressor": CatBoostRegressor(verbose=False)
+#             }
+            
+#             model_report = evaluate_models(X_train=X_train, y_train=y_train, X_validate=X_validate, 
+#                                            y_validate=y_validate,models=models)
+            
+            
+#             # Use the utility function to get the best model based on validation scores
+#             best_model_name, best_model_score = self.get_best_model_from_report(model_report)
+            
+            
+#             if best_model_score < 0.6:
+#                 raise CustomException("No best model found with sufficient score")
+            
+#             logging.info(f"Best model: {best_model_name} with R^2 score: {best_model_score}")
+
+#             # Access the best model using its name
+#             best_model = models[best_model_name]
+#             best_model.fit(X_train, y_train)  # Ensure the best model is retrained on the full training set
+            
+#             save_object(
+#                 file_path=self.model_trainer_config.trained_model_file_path,
+#                 obj=best_model
+#             )
+            
+#             predicted = best_model.predict(X_validate)
+#             r2_square = r2_score(y_validate, predicted)
+            
+#             return r2_square, best_model_name
+            
+        
+#         except Exception as e:
+#             raise CustomException(e, sys)
+
+
+"""
+The function is for Hyper-Parameter. The reason why i am not using it, is because
+the highest model score is lower than the highest model score without Hyper-parameter.     
+"""
+
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config = ModelTrainerConfig()
+        self.model_trainer_config = ModelTrainerConfig()   
         
-    def get_best_model_from_report(self, model_report):
-        # Extract only validation scores and model names
-        validation_scores = {key.replace('_validation_score', ''): score for key, score in model_report.items() if '_validation_score' in key}
-       
-        # Find the model with the highest validation score
-        best_model_name = max(validation_scores, key=validation_scores.get)
-        best_model_score = validation_scores[best_model_name]
         
-        return best_model_name, best_model_score
-    
     def initiate_model_trainer(self, train_array, validate_array):
         
         try:
@@ -47,6 +104,7 @@ class ModelTrainer:
             
             X_train, y_train = train_array[:, :-1], train_array[:, -1]
             X_validate, y_validate = validate_array[:, :-1], validate_array[:, -1]
+            # X_test, y_test = test_array[:, :-1], test_array[:, -1]
 
             models = {
                 "Random Forest": RandomForestRegressor(),
@@ -55,32 +113,41 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False)
             }
             
+            params = {
+            "Random Forest": {'n_estimators': [100, 200], 'max_depth': [10, 20], 'min_samples_split': [2, 5]},
+            "Gradient Boosting": {'learning_rate': [0.01, 0.1], 'n_estimators': [100, 200], 'max_depth': [3, 5]},
+            "XGBRegressor": {'learning_rate': [0.01, 0.1], 'n_estimators': [100, 200], 'max_depth': [3, 5]},
+            "CatBoosting Regressor": {'learning_rate': [0.01, 0.1], 'depth': [4, 6], 'iterations': [100, 200]}
+            }
+            
             model_report = evaluate_models(X_train=X_train, y_train=y_train, X_validate=X_validate, 
-                                           y_validate=y_validate,models=models)
+                                           y_validate=y_validate,models=models,param=params)
             
             
-            # Use the utility function to get the best model based on validation scores
-            best_model_name, best_model_score = self.get_best_model_from_report(model_report)
-            
-            
-            if best_model_score < 0.6:
-                raise CustomException("No best model found with sufficient score")
-            
-            logging.info(f"Best model: {best_model_name} with R^2 score: {best_model_score}")
-
-            # Access the best model using its name
-            best_model = models[best_model_name]
-            best_model.fit(X_train, y_train)  # Ensure the best model is retrained on the full training set
-            
-            save_object(
-                file_path=self.model_trainer_config.trained_model_file_path,
-                obj=best_model
-            )
-            
-            predicted = best_model.predict(X_validate)
-            r2_square = r2_score(y_validate, predicted)
-            
-            return r2_square, best_model_name
+            # Check if best_model information is present in the report
+            if 'best_model' in model_report and model_report['best_model']['score'] is not None:
+                best_model_name = model_report['best_model']['name']
+                best_model_score = model_report['best_model']['score']
+                
+                if best_model_score < 0.6:
+                    raise CustomException("No best model found with sufficient score")
+                
+                logging.info(f"Best model: {best_model_name} with R^2 score: {best_model_score}")
+                
+                # Access and retrain the best model using its name
+                best_model = models[best_model_name].fit(X_train, y_train)
+                
+                save_object(
+                    file_path=self.model_trainer_config.trained_model_file_path,
+                    obj=best_model
+                )
+                
+                predicted = best_model.predict(X_validate)
+                r2_square = r2_score(y_validate, predicted)
+                
+                return r2_square, best_model_name
+            else:
+                raise CustomException("Failed to identify the best model from evaluation.")
             
         
         except Exception as e:
--- BlackFriday/src/utils.py
+++ BlackFriday/src/utils.py
@@ -12,7 +12,10 @@ from sklearn.metrics import (
 )
 
 
+from sklearn.model_selection import RandomizedSearchCV
+
 from src.exception import CustomException
+from src.logger import logging
 
 
 def save_object(file_path, obj):
@@ -32,35 +35,92 @@ def save_object(file_path, obj):
     
     
     
-def evaluate_models(X_train, y_train, X_validate, y_validate, models):
-    try:
-        report = {}
+# def evaluate_models(X_train, y_train, X_validate, y_validate, models):
+#     try:
+#         report = {}
         
+#         for model_name, model in models.items():
+#         # for i in range(len(list(models))):
+            
+#         #     model = list(models.values())[i]
+            
+#             model.fit(X_train, y_train)
+            
+#             y_train_pred = model.predict(X_train)
+#             y_validate_pred = model.predict(X_validate)
+            
+            
+#             train_model_score = r2_score(y_train, y_train_pred)
+#             validation_model_score = r2_score(y_validate, y_validate_pred)
+            
+#             # report[model_name] = {
+#             #     'train_score': train_model_score,
+#             #     'validation_score': validation_model_score
+#             # }
+            
+#             report[f'{model_name}_train_score'] = train_model_score
+#             report[f'{model_name}_validation_score'] = validation_model_score
+            
+#             # report[list(models.keys())[1]] = validation_model_score
+
+#         return report
+           
+#     except Exception as e:
+#         raise CustomException(e, sys) 
+
+
+
+"""
+This function is used to evalute hyper-parameter model.
+"""   
+    
+def evaluate_models(X_train, y_train, X_validate, y_validate, models, param):
+    report = {}
+    best_model_name = None
+    best_model_score = -np.inf
+    try:
         for model_name, model in models.items():
-        # for i in range(len(list(models))):
-            
-        #     model = list(models.values())[i]
-            
-            model.fit(X_train, y_train)
-            
-            y_train_pred = model.predict(X_train)
-            y_validate_pred = model.predict(X_validate)
-            
-            
-            train_model_score = r2_score(y_train, y_train_pred)
+            logging.info(f"Processing model: {model_name}")
+            if model_name in param:  # Check if there are hyperparameters defined for the model
+                # Perform hyperparameter tuning
+                random_search = RandomizedSearchCV(model, param[model_name], cv=3, n_iter=8,
+                                                   scoring='r2', verbose=1, n_jobs=-1, random_state=42)
+                random_search.fit(X_train, y_train)
+                best_model = random_search.best_estimator_
+                best_params = random_search.best_params_
+            else:
+                # Train the model with default parameters
+                model.fit(X_train, y_train)
+                best_model = model
+                best_params = 'Default parameters'
+                # Logging success
+                logging.error(f"Exception occurred while processing model {model_name}: {str(e)}")
+
+            # Evaluate the best model (tuned or default) on the validation data
+            y_validate_pred = best_model.predict(X_validate)
             validation_model_score = r2_score(y_validate, y_validate_pred)
             
-            # report[model_name] = {
-            #     'train_score': train_model_score,
-            #     'validation_score': validation_model_score
-            # }
-            
-            report[f'{model_name}_train_score'] = train_model_score
-            report[f'{model_name}_validation_score'] = validation_model_score
-            
-            # report[list(models.keys())[1]] = validation_model_score
+            # Update the report with the validation score and best parameters
+            report[model_name] = {
+                'validation_score': validation_model_score,
+                'best_params': best_params
+            }
 
+            if validation_model_score > best_model_score:
+                best_model_score = validation_model_score
+                best_model_name = model_name
+
+        
+        if best_model_name:
+            # Append the best model information to the report if we found a best model
+            report['best_model'] = {
+                'name': best_model_name,
+                'score': best_model_score,
+                'parameters': report[best_model_name]['best_params']
+            }
+        else:
+            logging.error("No best model found. Please check the model configurations and data.")
         return report
            
     except Exception as e:
-        raise CustomException(e, sys) 
+        raise CustomException(e, sys)
